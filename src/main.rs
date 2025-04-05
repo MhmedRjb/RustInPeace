a5f4b89c4784f5f84abde892d3a69b02aeefdb7e
mhmedrjb <mohamed2elfateh@gmail.com>
2025-04-05 22:37:43 +0200

    Refactor main function to filter even numbers and calculate their sum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+
+// let is_even = |val:i32|-> bool {
+//     if (val % 2 == 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// };
 fn main(){
-    let mut sum :i32=0;
-    let add =|n1:i32,n2:i32| n1+n2;
-    let numbers=3..10;
-    for n in numbers.into_iter(){
+    let mut sum=0;
+    let add=|n1:i32 ,n2:i32| n1+n2;
+    let numbers=(3..12)
+        .inspect(|n| println!("before filter  = {}",n))
+        .filter(|n| n%2 == 0 )
+        .inspect(|n| println!("after filter n = {}",n));
+n
+    for n in numbers{
         sum=add(sum,n);
     }
-    println!("sum = {}", sum);
+    println!("sum = {}",sum);
 }
